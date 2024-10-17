bc97552b99ce337fd2d39ef72a53d02ee7018b34
Kohl <mikaorador@gmail.com>
2024-10-17 11:17:06 +0800

    fixed chatmenu

--- src/pages/ChatMenuPage.jsx
+++ src/pages/ChatMenuPage.jsx
@@ -12,10 +12,8 @@ import { db, auth } from "../configs/firebaseConfig";
 import Navbar from "../components/Navbar";  
 import Sidebar from "../components/Sidebar"; 
 import "../styles/chatmenu.css";
-import NavBar from "../components/Navbar.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock } from "@fortawesome/free-solid-svg-icons";
-import { faComment } from "@fortawesome/free-solid-svg-icons";
+import { faLock, faComment } from "@fortawesome/free-solid-svg-icons";
 
 const ChatMenuPage = ({ setRoom }) => {
   const [rooms, setRooms] = useState([]);
@@ -100,7 +98,6 @@ const ChatMenuPage = ({ setRoom }) => {
   };
 
   return (
-<<<<<<< HEAD
     <div className="chat-menu-page">
       <Navbar />  {/* Add Navbar at the top */}
       <div className="content-wrapper">
@@ -168,91 +165,6 @@ const ChatMenuPage = ({ setRoom }) => {
                 )}   
               </div>
             ))}
-=======
-    <div>
-      <NavBar />
-      <div className="bg-chat">
-        <div className="chat-menu-container">
-          <div className="chat-container">
-            <h1>Chatrooms</h1>
-            <div className="room-list">
-              {rooms.map((room) => (
-                <div key={room.id} className="room-item">
-                  <h3>{room.name}</h3>
-                  {room.password && (
-                    <span>
-                      <FontAwesomeIcon icon={faLock} />
-                    </span>
-                  )}
-                  <button onClick={() => handleJoinRoom(room.name)}>
-                    Join
-                  </button>
-                </div>
-              ))}
-            </div>
-
-            <div className="new-room-form">
-              <form onSubmit={handleCreateRoom}>
-                <h3>Create a new Room</h3>
-                <input
-                  type="text"
-                  placeholder="Room Name"
-                  value={newRoomName}
-                  onChange={(e) => setNewRoomName(e.target.value)}
-                  required
-                />
-                <div className="protect-checkbox">
-                  <input
-                    type="checkbox"
-                    checked={isPasswordProtected}
-                    onChange={(e) => setIsPasswordProtected(e.target.checked)}
-                  />
-                  <label>Password Protected</label>
-                </div>
-                {isPasswordProtected && (
-                  <input
-                    type="password"
-                    placeholder="Enter Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                )}
-                <button type="submit">Create Room</button>
-              </form>
-            </div>
-          </div>
-
-          <div className="forum-container">
-            <h1>Forum</h1>
-            <form onSubmit={handleCreatePost} className="post-form">
-              <input
-                type="text"
-                placeholder="Write a post..."
-                value={newPost}
-                onChange={(e) => setNewPost(e.target.value)}
-                required
-              />
-              <button type="submit">Post</button>
-            </form>
-
-            <div className="post-list">
-              {posts.map((post) => (
-                <div key={post.id} className="post-item">
-                  <small>
-                    <FontAwesomeIcon icon={faComment} /> {post.content}
-                  </small>
-                  <p>
-                    Posted by {post.createdBy} on{" "}
-                    {new Date(post.createdAt).toLocaleString()}
-                  </p>
-                  {post.createdById === currentUser.uid && ( // Check if the post was created by the current user
-                    <p className="your-post"> (Your Post)</p>
-                  )}
-                </div>
-              ))}
-            </div>
->>>>>>> 369d690ab69d5f35d088f74ecacf536e15868b72
           </div>
         </div>
       </div>
